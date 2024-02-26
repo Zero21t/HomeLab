1b08a8ccca7f98bccb7a2616f627f6775d2ca68f
Aman Toor <123129010+Zero21t@users.noreply.github.com>
2024-02-25 19:53:51 -0500

    Add files via upload

--- Docker-Tools.sh
+++ Docker-Tools.sh
@@ -5,5 +5,5 @@ sudo apt install curl -y
 sudo apt install net-tools -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker.io docker-ce docker-ce-cli containerd.io
 
